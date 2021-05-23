50fb4bfbacf93d96f7aeccfca1ec48f7b0b569ad
ZENENG FAN ZENENG FAN <fanzen@seas.upenn.edu>
2021-05-23 20:58:57 +0000

    add function content

--- hash_collision.py
+++ hash_collision.py
@@ -1,5 +1,6 @@
 import hashlib
-import os
+import string
+import random
 
 def hash_collision(k):
     if not isinstance(k,int):
@@ -8,10 +9,29 @@ def hash_collision(k):
     if k < 0:
         print( "Specify a positive number of bits" )
         return( b'\x00',b'\x00' )
-   
+    if k > 256:
+        print( "Specify a smaller number" )
+        return( b'\x00',b'\x00' )
     #Collision finding code goes here
-    x = b'\x00'
-    y = b'\x00'
+    end_length = 256
+    notEqual =  True
+    while(notEqual):
+        x = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 50)).encode('utf-8')
+        y = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 50)).encode('utf-8')
+        hx = hashlib.sha256(x)
+        hy = hashlib.sha256(y)
+        #compute hash in hex
+        hx16 = hx.hexdigest()
+        hy16 = hy.hexdigest()
+        #convert hex into binary string
+        hx2 = bin(int(hx16, 16))[2:].zfill(end_length)
+        hy2 = bin(int(hy16, 16))[2:].zfill(end_length)
+        hx_trailing = hx2[-k:]
+        #print("x:", hx_trailing)
+        hy_trailing = hy2[-k:]
+        #print("y:", hy_trailing)
+        if hx_trailing == hy_trailing:
+            notEqual = False
     
     return( x, y )
 
